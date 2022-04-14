4409bcefeb9b434219027cb124ad1ffd955e6b38
Garush Nahapetyan <gng85@mail.ru>
2022-04-14 12:59:42 +0400

    add selectors

---
+++ my-new-app/src/store/selectors/index.js
@@ -0,0 +1,23 @@
+export const loginStatusSelector = (state) => {
+    return state.LoginStatus;
+};
+
+export const membersListSelector = (state) => {
+    return state.members;
+};
+
+export const projectsListSelector = (state) => {
+    return state.projects;
+};
+export const tasksListSelector = (state) => {
+    return state.tasks;
+};
+
+export const mainPageSelector = (state) => {
+    tasks
+    return {
+        logerMember: state.logerMember,
+        members: state.mambers,
+        projects: stateProjects,
+    };
+};
