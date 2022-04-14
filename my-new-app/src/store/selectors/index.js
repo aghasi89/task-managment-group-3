90b66c4f4b27c0bf44566ef5e6d6a46a202ea8c4
Garush Nahapetyan <gng85@mail.ru>
2022-04-14 13:02:13 +0400

    updet selectors

--- my-new-app/src/store/selectors/index.js
+++ my-new-app/src/store/selectors/index.js
@@ -14,7 +14,6 @@ export const tasksListSelector = (state) => {
 };
 
 export const mainPageSelector = (state) => {
-    tasks
     return {
         logerMember: state.logerMember,
         members: state.mambers,
