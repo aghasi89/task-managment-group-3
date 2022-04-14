7c7759442c63b5f908495d1ffa8fd93276ce7594
Garush Nahapetyan <gng85@mail.ru>
2022-04-14 12:38:34 +0400

    adding Provider and update initial state

--- my-new-app/src/store/redusers/loginReduser.js
+++ my-new-app/src/store/redusers/loginReduser.js
@@ -2,16 +2,13 @@ import types from "../types";
 
 const INIT_STATE = {
     LoginStatus: false,
-    logerMember:{},
     members: [
         {
             name: "Admin",
-            emai: "admin.damin@gmail.com",
+            email: "admin.damin@gmail.com",
             pasword: "****",
         }
     ],
-    projects:[],
-    tasks:[],
 };
 
 const reducer = (state = INIT_STATE, action) => {
